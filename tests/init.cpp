e9f9345cbf18789504c2b5c44fec9885f60c52ac
Ivanopulopulo <vkuvshinov.ru@gmail.com>
2017-05-15 21:23:16 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -7,7 +7,7 @@ SCENARIO("mas")
 	int m[] = { 6,3,8,2,1 };
 	int* f = m;
 	int* l=m+4;
-	quick_Sort(f, l);
+	q_Sort(f, l);
 	REQUIRE(m[0] == 1);
 	REQUIRE(m[1] == 2);
 	REQUIRE(m[2] == 3);
@@ -19,7 +19,7 @@ SCENARIO("matrix")
  	int mas[3][3] = { 21,5,78,34,27,6,87,11,77 };
 	int * first = &mas[0][0];
 	int * last = &mas[2][2];
-	quick_Sort(first, last);
+	q_Sort(first, last);
 	REQUIRE(mas[0][0] == 5);
 	REQUIRE(mas[0][1] == 6);
 	REQUIRE(mas[0][2] == 11);
